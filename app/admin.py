0f6fb20f98b18a246f6456f011fa3896d8cea426
Vishnu <vishnushaji179@gmail.com>
2024-02-05 11:05:50 +0530

    templates edit

--- app/admin.py
+++ app/admin.py
@@ -1,28 +1,12 @@
 from django.contrib import admin
-from .models import Product, Category, BlogPost, Testimonial,OrderItem,Order
+from .models import Product, Category, BlogPost, Testimonial,OrderItem,Order,CartItem
 
 
 
 
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 1
+admin.site.register(CartItem)
 
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'get_total_quantity', 'get_total_price', 'created_at']
-    inlines = [OrderItemInline]
-
-    def get_total_quantity(self, obj):
-        return obj.total_quantity()
-
-    def get_total_price(self, obj):
-        return obj.total_price()
-
-    get_total_quantity.short_description = 'Total Quantity'
-    get_total_price.short_description = 'Total Price'
-
-admin.site.register(Order, OrderAdmin)
 
 
 
